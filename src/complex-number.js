8c478d062c2846670801719c30bac17c8f696503
Utsab Bera <nCoder24@users.noreply.github.com>
2023-05-03 09:48:20 +0530

    refactored toString function

--- src/complex-number.js
+++ src/complex-number.js
@@ -2,13 +2,8 @@ const createComplexNumber = function(realPart, imaginaryPart) {
   const toString = function() {
     if(!realPart) return `${imaginaryPart}i`;
 
-    let stringified = "";
-
-    stringified += realPart;
-    stringified += imaginaryPart < 0 ? " - " : " + ";
-    stringified += Math.abs(imaginaryPart) + "i"; 
-
-    return stringified;
+    const operator = imaginaryPart < 0 ? "-" : "+";
+    return`${realPart} ${operator} ${Math.abs(imaginaryPart)}i`; 
   }
 
   const getImaginaryPart = function() {
